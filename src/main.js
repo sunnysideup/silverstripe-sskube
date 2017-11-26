8c5963b76b8b0140d7e97c83be305debac2fd90e
Nicolaas <github@sunnysideup.co.nz>
2017-11-27 12:16:48 +1300

    MAJOR: upgrading to kube version 6.5.2

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@
 import './sass/style.sass';
 
 
-/** JS */
-import Kube from 'base/js/kube/Core/Kube';
-import Plugin from 'base/js/kube/Core/Kube.Plugin';
-import Animation from 'base/js/kube/Core/Kube.Animation';
-import Detect from 'base/js/kube/Core/Kube.Detect';
-import FormData from 'base/js/kube/Core/Kube.Formdata';
-import Response from 'base/js/kube/Core/Kube.Response';
-import Utils from 'base/js/kube/Core/Kube.Utils';
-import Message from 'base/js/kube/Message/Kube.Message';
-import Sticky from 'base/js/kube/Sticky/Kube.Sticky';
-import Toggleme from 'base/js/kube/Toggleme/Kube.Toggleme';
-import Offcanvas from 'base/js/kube/Offcanvas/Kube.Offcanvas';
-import Collapse from 'base/js/kube/Collapse/Kube.Collapse';
-import Dropdown from 'base/js/kube/Dropdown/Kube.Dropdown';
-import Tabs from 'base/js/kube/Tabs/Kube.Tabs';
-import Modal from 'base/js/kube/Modal/Kube.Modal';
-
-import Menu from 'base/js/Menu/Menu';
+// /** JS */
+// import Kube from 'base/js/kube/Core/Kube';
+// import Plugin from 'base/js/kube/Core/Kube.Plugin';
+// import Animation from 'base/js/kube/Core/Kube.Animation';
+// import Detect from 'base/js/kube/Core/Kube.Detect';
+// import FormData from 'base/js/kube/Core/Kube.Formdata';
+// import Response from 'base/js/kube/Core/Kube.Response';
+// import Utils from 'base/js/kube/Core/Kube.Utils';
+// import Message from 'base/js/kube/Message/Kube.Message';
+// import Sticky from 'base/js/kube/Sticky/Kube.Sticky';
+// import Toggleme from 'base/js/kube/Toggleme/Kube.Toggleme';
+// import Offcanvas from 'base/js/kube/Offcanvas/Kube.Offcanvas';
+// import Collapse from 'base/js/kube/Collapse/Kube.Collapse';
+// import Dropdown from 'base/js/kube/Dropdown/Kube.Dropdown';
+// import Tabs from 'base/js/kube/Tabs/Kube.Tabs';
+// import Modal from 'base/js/kube/Modal/Kube.Modal';
+//
+// import Menu from 'base/js/Menu/Menu';
